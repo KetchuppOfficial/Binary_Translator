44a0b3ac46e0d82d8dfb286c5c59dfec9944b77b
KetchuppOfficial <dolgov04@list.ru>
2022-05-04 18:53:54 +0300

    Header of the project

---
+++ Binary_Translator.h
@@ -0,0 +1,15 @@
+#ifndef BINARY_TRANSLATOR_INSLUDED
+#define BINARY_TRANSLATOR_INSLUDED
+
+#include "../../My_Lib/My_Lib.h"
+
+#define DEBUG 1
+
+#if DEBUG == 0
+#undef MY_ASSERT
+#define MY_ASSERT(condition, var, err_num, error) ;
+#endif
+
+int Translate (const char *const input, const char *const output);
+
+#endif
