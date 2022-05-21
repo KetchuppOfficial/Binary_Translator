a8f6e9d0bf32ceb2c45710e631873cc02a2caec7
KetchuppOfficial <dolgov04@list.ru>
2022-05-21 18:32:01 +0300

    Checking argc is implemented as a separated function; Binary_Translator receives only one argument since now

--- src/main.c
+++ src/main.c
@@ -1,25 +1,25 @@
 #include "../include/Binary_Translator.h"
 
+int Check_Argc (int argc)
+{
+    return (argc < 2) ? 1 : 0;
+}
+
 int main (int argc, char *argv[])
 {
     #ifdef DEBUG
     OPEN_LOG_FILE;
     #endif
     
-    if (argc < 3)
-    {
-        MY_ASSERT (false, "int argc", NOT_ENOUGH_ARGS, ERROR);
-    }
-    else
-    {
-        #ifdef DEBUG
-        int ret_val = Binary_Translator (argv[1], argv[2]);
-        #else
-        Binary_Translator (argv[1], argv[2]);
-        #endif
+    MY_ASSERT (Check_Argc (argc) == 0, "int argc", NOT_ENOUGH_ARGS, ERROR);
 
-        MY_ASSERT (ret_val != ERROR, "Translate ()", FUNC_ERROR, ERROR);
-    }
+    #ifdef DEBUG
+    int ret_val = Binary_Translator (argv[1], argv[2]);
+    #else
+    Binary_Translator (argv[1]);
+    #endif
+
+    MY_ASSERT (ret_val != ERROR, "Translate ()", FUNC_ERROR, ERROR);
     
     return 0;
 }
