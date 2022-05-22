63b0178cbcd6b390dc69a52a8de565ec178ee97f
KetchuppOfficial <dolgov04@list.ru>
2022-05-22 03:17:47 +0300

    Instruction names were written in Caps

--- include/Commands_List.h
+++ include/Commands_List.h
@@ -1,37 +1,37 @@
-DEFCMD_(0,  hlt,  0)
+DEFCMD_(0x00, HLT,  0)
 
-DEFCMD_(1,  call, 1)
+DEFCMD_(0x01, CALL, 1)
 
-DEFCMD_(2,  jmp,  1)
+DEFCMD_(0x02, JMP,  1)
 
-DEFCMD_(3,  jae,  1)
+DEFCMD_(0x03, JAE,  1)
 
-DEFCMD_(4,  ja,   1)
+DEFCMD_(0x04, JA,   1)
 
-DEFCMD_(5,  jbe,  1)
+DEFCMD_(0x05, JBE,  1)
 
-DEFCMD_(6,  jb,   1)
+DEFCMD_(0x06, JB,   1)
 
-DEFCMD_(7,  je,   1)
+DEFCMD_(0x07, JE,   1)
 
-DEFCMD_(8,  jne,  1)
+DEFCMD_(0x08, JNE,  1)
 
-DEFCMD_(9,  ret,  0)
+DEFCMD_(0x09, RET,  0)
 
-DEFCMD_(10, in,   0)
+DEFCMD_(0x0A, IN,   0)
 
-DEFCMD_(11, out,  0)
+DEFCMD_(0x0B, OUT,  0)
 
-DEFCMD_(12, push, 1)
+DEFCMD_(0x0C, PUSH, 1)
 
-DEFCMD_(13, pop,  1)
+DEFCMD_(0x0D, POP,  1)
 
-DEFCMD_(14, add,  0)
+DEFCMD_(0x0E, ADD,  0)
 
-DEFCMD_(15, sub,  0)
+DEFCMD_(0x0F, SUB,  0)
 
-DEFCMD_(16, mul,  0)
+DEFCMD_(0x10, MUL,  0)
 
-DEFCMD_(17, dvd,  0)
+DEFCMD_(0x11, DVD,  0)
 
-DEFCMD_(18, Sqrt, 0)
+DEFCMD_(0x12, SQRT, 0)
