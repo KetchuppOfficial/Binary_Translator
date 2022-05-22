ec974e591b288e9f69438c6ef6a6efca9c4172b6
KetchuppOfficial <dolgov04@list.ru>
2022-05-22 03:18:55 +0300

    3 enums from Binary_Translator.c relocated here

--- include/Binary_Translator.h
+++ include/Binary_Translator.h
@@ -11,6 +11,72 @@
 #define MY_ASSERT(condition, var, err_num, error) ;
 #endif
 
+#define DEFCMD_(num, name, n_args) name = num,
+
+enum Instructions
+{
+    #include "../include/Commands_List.h"
+};
+
+#undef DEFCMD_
+
+#define N_INSTRUCTIONS 28
+
+enum ISA
+{
+    hlt,
+    call,
+    jmp,
+    jae,
+    ja,
+    jbe,
+    jb,
+    je,
+    jne,
+    ret,
+    in_aligned,
+    in_unaligned,
+    out,
+    push_num,
+    push_ram_num,
+    push_reg,
+    push_ram_reg,
+    push_ram_reg_num,
+    pop,
+    pop_ram_num,
+    pop_reg,
+    pop_ram_reg,
+    pop_ram_reg_num,
+    add,
+    sub,
+    mul,
+    dvd,
+    Sqrt
+};
+
+enum PUSH_POP
+{
+    EMPTY      =   0,   // pop
+
+    AX         =  10,   // push/pop ax
+    BX         =  20,   // push/pop bx
+    CX         =  30,   // push/pop cx
+    DX         =  40,   // push/pop dx
+
+    RAM_AX     =  11,   // push/pop [ax]
+    RAM_BX     =  21,   // push/pop [bx]
+    RAM_CX     =  31,   // push/pop [cx]
+    RAM_DX     =  41,   // push/pop [dx]
+
+    NUM        = 100,   // push 4
+    RAM_NUM    = 101,   // push/pop [4]
+
+    RAM_AX_NUM = 111,   // push/pop [ax + 4]
+    RAM_BX_NUM = 121,   // push/pop [bx + 4]
+    RAM_CX_NUM = 131,   // push/pop [cx + 4]
+    RAM_DX_NUM = 141,   // push/pop [dx + 4]   
+};
+
 int Binary_Translator (const char *const input);
 
 #endif
